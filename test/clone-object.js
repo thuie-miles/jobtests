bf286ad0b8e704d2710d8c3d49ee850b9ca1b96c
thuie-miles <53225740+thuie-miles@users.noreply.github.com>
2019-07-23 15:53:31 +0100

    Update clone-object.js

--- test/clone-object.js
+++ test/clone-object.js
@@ -2,6 +2,12 @@ describe('clone object', function () {
   it('should clone an object', function () {
     var expected = {name: 'Ahmed', age: 27, skills: ['cycling', 'walking', 'eating']},
         obj = {};
+    
+    function cloning() {
+          return JSON.parse(JSON.stringify(expected));
+        }
+
+        obj = cloning(obj);
 
     expect(obj).toEqual(expected);
     expect(obj).not.toBe(expected);
